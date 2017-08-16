be2bb297c7ba2a4f7dbb3d64d3a6212dae1ff908
LanScom <930371781@qq.com>
2017-08-17 00:10:32 +0800

    验证Output.WirteLine功能

--- C入门/Program.cs
+++ C入门/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
-
+using System.Diagnostics;
 namespace C入门
 {
     class Program
@@ -62,6 +62,7 @@ namespace C入门
             {
                 Console.WriteLine("{0}", word);
             }
+            /*
             processDelegate process;
             Console.WriteLine("Enter 2 numbers separated with a comma:");
             string input = Console.ReadLine();
@@ -75,7 +76,53 @@ namespace C入门
             else
                 process = new processDelegate(divide);
             Console.WriteLine("result:{0}", process(param1, param2));
+             */
+            int[] testArray = { 4,7,4,2,7,3,7,8,3,9,1,9};
+            int[] maxValIndices;
+            int maxVal = Maxima(testArray, out maxValIndices);
+            Console.WriteLine("Maximum value {0} found at element indices:",maxVal);
+            foreach (int index in maxValIndices)
+            {
+                Console.WriteLine(index);
+            }
             Console.ReadKey();
-}
-}
+  }
+   static int Maxima(int[] integers,out int[]indices)
+  {
+            Debug.WriteLine("maximum value search started.");
+            indices = new int[1];
+            int maxVal = integers[0];
+            indices[0] = 0;
+            int count = 1;
+            Debug.WriteLine(string.Format("Maximum value initialized to {0}, at element index 0.", maxVal));
+            for (int i = 1; i < integers.Length; i++)
+            {
+                Debug.WriteLine(string.Format("Now looking at element at index {0}.", i));
+                if (integers[i] > maxVal)
+                {
+                    maxVal = integers[i];
+                    count = 1;
+                    indices = new int[1];
+                    indices[0] = i;
+                    Debug.WriteLine(string.Format("New maximum found. New value is {0}, at element index {1}.", maxVal, i));
+                }
+                else
+                {
+                    if (integers[i] == maxVal)
+                    {
+                        count++;
+                        int[] oldIndices = indices;
+                        indices = new int[count];
+                        oldIndices.CopyTo(indices, 0);
+                        indices[count - 1] = i;
+                        Debug.WriteLine(string.Format("Duplicate maximum found at element index {0}.", i));
+
+                    }
+                }
+            }
+        Trace.WriteLine(string.Format("Maximum value {0} found, with {1} occurrences.", maxVal, count));
+        Debug.WriteLine("Maximum value search completed.");
+        return maxVal;
+   }
+ }
 }
